61c4de9c327f0fdb144045fc409f46be9e121ea1
arcinston <arushkurundodi@gmail.com>
2022-10-26 18:44:41 +0530

    git init

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,21 @@ import {
 import PeerVideoAudioElem from "./components/PeerVideoAudioElem";
 
 function App() {
-  const huddleClient = getHuddleClient("YOUR_API_KEY");
+  const huddleClient = getHuddleClient("arcinston");
   const stream = useRootStore((state) => state.stream);
   const enableStream = useRootStore((state) => state.enableStream);
   const pauseTracks = useRootStore((state) => state.pauseTracks);
   const isCamPaused = useRootStore((state) => state.isCamPaused);
   const peers = useRootStore((state) => state.peers);
   const peerId = useRootStore((state) => state.peerId);
+  const displayName = useRootStore((state) => state.displayName);
   const lobbyPeers = useRootStore((state) => state.lobbyPeers);
   const roomState = useRootStore((state) => state.roomState);
-
-  const handleJoin = async () => {
+  const [roomName,setRoomName]= useState('');
+  const handleJoin = async (roomId:string) => {
     try {
-      await huddleClient.join("dev ", {
+      
+      await huddleClient.join(roomId, {
         address: "0x15900c698ee356E6976e5645394F027F0704c8Eb",
         wallet: "",
         ens: "axit.eth",
@@ -33,6 +35,8 @@ function App() {
       console.log({ error });
     }
   };
+ 
+
 
   const videoRef = useRef<HTMLVideoElement>(null);
 
@@ -47,57 +51,29 @@ function App() {
   }, [peers, peerId, isCamPaused]);
 
   return (
-    <HuddleClientProvider value={huddleClient}>
-      <div className="App grid grid-cols-2">
-        <div>
-          <div>
-            <a href="https://vitejs.dev" target="_blank">
-              <img src="/vite.svg" className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://reactjs.org" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
-          <h1>Vite + React</h1>
-
+    <HuddleClientProvider value={huddleClient}  >
           <h2 className={`text-${!roomState.joined ? "red" : "green"}`}>
-            Room Joined:&nbsp;{roomState.joined.toString()}
+            Room Joined:&nbsp;{roomState.joined?roomState.roomId:"Not Joined"}
           </h2>
-          <h2>Instructions</h2>
-          <ol className="w-fit mx-auto text-left">
-            <li>
-              Click on <b>Enable Stream</b>
-            </li>
-            <li>
-              Then Click on <b>Join room</b>, <i>"Room Joined"</i> should be
-              changed to true
-            </li>
-            <li>
-              Open the app in a <b>new tab</b> and repeat <b>steps 1 & 2</b>
-            </li>
-            <li>Return to 1st tab, now you'll see peers in the peer list,</li>
-            <li>
-              Click on <b>allowAllLobbyPeersToJoinRoom</b> to accept peers into
-              the room.
-            </li>
-          </ol>
-        </div>
+          {
+            !roomState.joined&&
+            (<div className="grid grid-cols-2 ">
+            <input  type="text"
+                value={roomName}
+                className="min-padding"
+                placeholder="Enter Room Name"
+                onChange={e => setRoomName(e.target.value)}
+                onKeyDown={e => {
+                  if (!roomName) return;
+                  if (e.key === 'Enter') setRoomName(e.target.value);
+                }  } 
+                />
+            <button onClick={()=>handleJoin(roomName)}>Join Room</button>
+          </div>)
+              }
+   
 
         <div>
-          <div className="card">
-            <button onClick={handleJoin}>Join Room</button>
-            <button onClick={() => enableStream()}>Enable Stream</button>
-            <button onClick={() => pauseTracks()}>Disable Stream</button>
-            <button onClick={() => huddleClient.enableWebcam()}>
-              Enable Webcam
-            </button>
-            <button onClick={() => huddleClient.disableWebcam()}>
-              Disable Webcam
-            </button>
-            <button onClick={() => huddleClient.allowAllLobbyPeersToJoinRoom()}>
-              allowAllLobbyPeersToJoinRoom()
-            </button>
-          </div>
           {!isCamPaused && (
             <video
               style={{ width: "50%" }}
@@ -122,7 +98,23 @@ function App() {
             ))}
           </div>
         </div>
-      </div>
+          <div className="btm-bar">
+            <button onClick={() => enableStream()}>Enable Stream</button>
+            <button onClick={() => pauseTracks()}>Disable Stream</button>
+            <button onClick={() => huddleClient.enableWebcam()}>
+              Enable Webcam
+            </button>
+            <button onClick={() => huddleClient.disableWebcam()}>
+              Disable Webcam
+            </button>
+          {
+          lobbyPeers[0] && (
+            <button onClick={() => huddleClient.allowAllLobbyPeersToJoinRoom()}>
+              All Lobby Peers
+            </button>
+          )
+          } 
+          </div>
     </HuddleClientProvider>
   );
 }
